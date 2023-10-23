54df4ae9764b79f810211a17c93d4a871548429c
Kumpan Ivan <kumpanvana@gmail.com>
2023-10-23 21:03:19 +0200

    Dodano plik .gitignore, który ignoruje folder out/ oraz dodano klasę Product.

---
+++ src/Category.java
@@ -0,0 +1,26 @@
+public class Category {
+    private String name;
+    public  Category (){
+        this.name ="Default Category";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name!=null && !name.isEmpty()){
+            this.name=name;
+        }else{
+            System.out.println("Invalid name!");
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Category category = new Category();
+        test(category);
+        category.setName("");
+        test(category);
+        category.setName(null);
+        test(category);
+        category.setName("Ivan");
+        test(category);
+        System.out.println();
+
+    }
+    public static void test(Category category ){
+        System.out.println(category.getName());
+        System.out.println(category);
+        category.toString();
+        System.out.println();
+    }
+}
